b6e343192f05775b072ce853ea50575d6f8f9470
bibek1414 <bbhattarai770@gmail.com>
2026-07-06 11:57:03 +0545

    feat: import AnimatePresence from framer-motion in ProjectsShowcase component

--- components/ProjectsShowcase.tsx
+++ components/ProjectsShowcase.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { X, ChevronRight, Maximize2, CheckCircle2, ArrowUpRight } from "lucide-react";
 import { projects } from "@/lib/data";
 
